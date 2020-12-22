3aa9917a3d91e46f1704a72893048be70d326447
Daniel McCarville <62717810+danielmccarville@users.noreply.github.com>
2020-12-21 19:52:24 -0600

    Update benfords.py
    Resolves Issue 1. Allows expectation() to calculate the probability of digit (d) appearing as the nth significant digit.

--- benfords.py
+++ benfords.py
@@ -1,15 +1,27 @@
 from math import log
 
 # Functions
-def expectation(d):
+def expectation(digit, position=1):
     """
     Given a non-zero integer, return the probability of a natural number starting with that digit as described by Benford's Law.
     """
-    if float(d).is_integer() is False:
+    if float(digit).is_integer() is False or float(position).is_integer() is False:
         raise TypeError
     else:
-        try:
-            term = (d+1)/d
-            return log(term, 10)
-        except ZeroDivisionError as e:
-            print(e)
+        if position == 1:
+            try:
+                term = (digit+1)/digit
+                return log(term, 10)
+            except ZeroDivisionError as e:
+                print(e)
+        if position > 1:
+            start = 10**(position-2)
+            stop = (10**(position-1))-1
+
+            value = 0
+            for i in range(start, stop+1):
+                term = 1+(1/((10*i)+digit))
+                value += log(term, 10)
+
+            return value
+
